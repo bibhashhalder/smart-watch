4fabf6094db04286c2691bb6e44d30fac6edd72d
bibhashhalder <halderbibhash80@gmail.com>
2024-10-13 10:06:50 +0600

    react icon added

--- src/app/components/ui/ProductCard.tsx
+++ src/app/components/ui/ProductCard.tsx
@@ -1,20 +1,23 @@
 "use client"
 import { IProducts } from '@/types';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import { AiFillLike } from 'react-icons/ai';
+import { FaCalendar } from 'react-icons/fa';
 
 const ProductCard = ({product}:{product:IProducts}) => {
-    const {title, publish_date, author_name, blog_image, description, total_likes} = product
+    const {id, title, publish_date, author_name, blog_image, description, total_likes} = product
     return (
         <div className='w-full mx-auto'>
            <div className='w-full border-2 p-2 rounded-md h-full'>
                <Image className='rounded-t-md w-full' src={blog_image} alt='product' width={500} height={500}/>
                 <h1 className='text-lg font-semibold'>{title}</h1>
                 <p className='text-xl font-semibold'>{author_name}</p>
-                <p className='text-sm'>{description}</p>
+                <p className='text-sm'>{description.length>100?description.slice(0,180):description}...<Link className='text-teal-500 font-bold' href={`/blogs/${id}`}>Read More</Link></p>
                 <div className='flex mt-2 justify-between gap-4 '>
-                    <p><span className='font-bold'>date:</span> {publish_date}</p>
-                    <p>{total_likes}</p>
+                    <p className='flex gap-2'><span className='font-bold'><FaCalendar className='text-teal-400'/></span> {publish_date}</p>
+                    <p className='flex gap-2'> <AiFillLike className='text-teal-400 text-xl'/>{total_likes}</p>
                 </div>
            </div>
         </div>
--- src/app/components/ui/SmallProductCard.tsx
+++ src/app/components/ui/SmallProductCard.tsx
@@ -1,14 +1,18 @@
+'use client'
 import { IProducts } from '@/types';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import { AiFillLike } from 'react-icons/ai';
 
 const SmallProductCard = ({product}:{product:IProducts}) => {
-    const {blog_image, description, total_likes}=product
+    console.log(product);
+    const {id,blog_image, description, total_likes}=product
     return (
         <div className='w-full border-2 p-2 rounded-md'>
             <Image className='w-full rounded-t-md h-3/5' src={blog_image} width={250} height={250} alt='small card'/>
-            <p>{description}</p>
-            <p>{total_likes}</p>
+            <p>{description.length>100?description.slice(0,180):description}...<Link className='text-teal-500 font-bold' href={`/blogs/${id}`}>Read More</Link></p>
+            <p className='flex gap-2'> <AiFillLike className='text-teal-400 text-xl'/>{total_likes}</p>
         </div>
     );
 };
